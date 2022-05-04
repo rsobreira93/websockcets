b4c4bc37ebc18f04253d607fd86202afac69b4d9
rsobreira93 <sobreiraromuloes@gmail.com>
2022-05-04 07:22:24 -0300

    feat: add CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,43 @@
+import { injectable } from "tsyringe";
+import { User } from "../schemas/User";
+
+interface ICreateUserDTO {
+  name: string;
+  email: string;
+  avatar: string;
+  socket_id: string;
+}
+
+@injectable()
+class CreateUserService {
+  async execute({ avatar, email, name, socket_id }: ICreateUserDTO) {
+    const userAlreadyExists = await User.findOne({ email }).exec();
+
+    if (userAlreadyExists) {
+      const user = await User.findOneAndUpdate(
+        {
+          _id: userAlreadyExists._id,
+        },
+        {
+          $set: {
+            socket_id,
+            avatar,
+            name,
+          },
+        }
+      );
+      return user;
+    } else {
+      const user = await User.create({
+        avatar,
+        email,
+        name,
+        socket_id,
+      });
+
+      return user;
+    }
+  }
+}
+
+export { CreateUserService };
